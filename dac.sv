ae8e257f4e57289d1ffe58eacbf26ac67fdf34cc
hiuhui10190201 <2622136284@qq.com>
2025-11-06 03:48:34 +0800

    8192

--- dac.sv
+++ dac.sv
@@ -1,12 +1,12 @@
 module dac(
 	input clk125,
 	input [7:0]din,
-	input [13:0]cycle,
+	input [12:0]cycle,
 	input [7:0]div,
 	output reg [9:0]dout,
-	output reg [13:0]addr);
+	output reg [12:0]addr);
 
-	reg [13:0]c;
+	reg [12:0]c;
 	reg [15:0]d, cnt;
 	always @(negedge clk125) begin
 		c <= cycle;
--- gowin_sdpb/rx2da_tmp.v
+++ gowin_sdpb/rx2da_tmp.v
@@ -5,7 +5,7 @@
 //Part Number: GW5A-LV25UG324ES
 //Device: GW5A-25
 //Device Version: A
-//Created Time: Tue Oct 28 01:09:32 2025
+//Created Time: Thu Nov  6 03:35:01 2025
 
 //Change the instance name and port connections to the signal names
 //--------Copy here to design--------
@@ -18,9 +18,9 @@
         .ceb(ceb_i), //input ceb
         .oce(oce_i), //input oce
         .reset(reset_i), //input reset
-        .ada(ada_i), //input [13:0] ada
+        .ada(ada_i), //input [12:0] ada
         .din(din_i), //input [7:0] din
-        .adb(adb_i) //input [13:0] adb
+        .adb(adb_i) //input [12:0] adb
     );
 
 //--------Copy end-------------------
